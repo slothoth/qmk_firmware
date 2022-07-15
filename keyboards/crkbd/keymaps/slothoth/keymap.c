b9b4d059a765ad7e91fc1d9c7f480b232c0558ac
Sam Mayo <sdm2000@hw.ac.uk>
2022-07-15 23:37:21 +0100

    switched sides of some values, need to add double quotes

--- keyboards/crkbd/keymaps/slothoth/keymap.c
+++ keyboards/crkbd/keymaps/slothoth/keymap.c
@@ -20,21 +20,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 
 #define _COLEMAKDHM 0 
-#define _NUMPAD 1
+#define _NUM 1
 #define _SYM 2
-#define _NAV 3
 
 // Layer toggle and switch
-#define T_NAV TT(_NAV)
-#define S_NAV MO(_NAV)
 #define T_SYM TT(_SYM)
-#define S_SYM MO(_SYM)
+#define T_NUM TT(_NUM)
+
 
 // Tap/mod keys
-#define RCTL_BR RCTL_T(KC_RBRACKET)
-#define LCTL_BR LCTL_T(KC_LBRACKET)
-#define SFT_SPC LSFT_T(KC_SPACE)
-#define SFT_ENT RSFT_T(KC_ENTER)
 // my mod taps
 #define CTL_TAB LCTL_T(KC_TAB)
 #define SFT_ESC LSFT_T(KC_ESC)
@@ -52,6 +46,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // €
 #define KC_EUR ALGR(KC_5)
 
+// Copy Paste Cut Undo Redo
+#define MY_CPY LCTL(KC_C)
+#define MY_PST LCTL(KC_V)
+#define MY_CUT LCTL(KC_X)
+#define MY_UNDO LCTL(KC_Z)
+#define MY_REDO LCTL(LSFT(KC_Z))
+
 // Tap Dance declarations
 enum {
     Q_,
@@ -60,79 +61,66 @@ enum {
     Y_AST,
     I_EQ,
     CMA_SCLN,
-    M_CLN,
+    J_CLN,
     H_DOL,
     PLS_MNS,
-    LPAR_LBR,
-    RPAR_RBR
+    LBR_RBR,
+    LPAR_RPAR,
+    TD_KAT
 };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [Q_] = ACTION_TAP_DANCE_DOUBLE(KC_Q, LSFT(KC_MINUS)),
-    [V_HASH] = ACTION_TAP_DANCE_DOUBLE(KC_V, KC_NONUS_HASH),
-    [U_QTE] = ACTION_TAP_DANCE_DOUBLE(KC_U, KC_QUOT),
-    [Y_AST] = ACTION_TAP_DANCE_DOUBLE(KC_Y, LSFT(KC_8)),
-    [I_EQ] = ACTION_TAP_DANCE_DOUBLE(KC_T, KC_EQL),
-    [H_DOL] = ACTION_TAP_DANCE_DOUBLE(KC_H, LSFT(KC_4)),
-    [M_CLN] = ACTION_TAP_DANCE_DOUBLE(KC_M, LSFT(KC_SCLN)),
-    [CMA_SCLN] = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_SCLN),
-    [PLS_MNS] = ACTION_TAP_DANCE_DOUBLE(LSFT(KC_EQL), KC_MINUS),
-    [LPAR_LBR] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, LSFT(KC_9)),
-    [RPAR_RBR] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, LSFT(KC_0))
+    [Q_]        = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_UNDS),
+    [V_HASH]    = ACTION_TAP_DANCE_DOUBLE(KC_V, KC_HASH),
+    [U_QTE]     = ACTION_TAP_DANCE_DOUBLE(KC_U, KC_QUOT),
+    [Y_AST]     = ACTION_TAP_DANCE_DOUBLE(KC_Y, KC_ASTR),
+    [I_EQ]      = ACTION_TAP_DANCE_DOUBLE(KC_I, KC_EQL),
+    [H_DOL]     = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_DLR),
+    [J_CLN]     = ACTION_TAP_DANCE_DOUBLE(KC_J, KC_COLN),
+    [CMA_SCLN]  = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_SCLN),
+    [PLS_MNS]   = ACTION_TAP_DANCE_DOUBLE(KC_PLUS, KC_MINUS),
+    [LBR_RBR]  = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
+    [LPAR_RPAR]  = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
+    [TD_KAT]      = ACTION_TAP_DANCE_DOUBLE(KC_K, LSFT(KC_QUOT))
+
 };
-
-
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAKDHM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------------.                     ,-----------------------------------------------------------.
-      KC_ESC,    TD(Q_),   KC_W,      KC_F,    KC_P,     KC_B,                            KC_J,    KC_L, TD(U_QTE), TD(Y_AST), TD(PLS_MNS), KC_BSPC,
+      KC_ESC,    TD(Q_),   KC_W,      KC_F,    KC_P,     KC_B,                         TD(J_CLN),    KC_L, TD(U_QTE), TD(Y_AST), TD(PLS_MNS), KC_BSPC,
   //|---------+---------+---------+---------+---------+---------|                     |---------+---------+---------+---------+---------+---------|
-      CTL_TAB,    KC_A,    KC_R,      KC_S,    KC_T,     KC_G,                         TD(M_CLN),   KC_N,   KC_E,    TD(I_EQ),   KC_O,  TD(LPAR_LBR),
+      CTL_TAB,    KC_A,    KC_R,      KC_S,    KC_T,     KC_G,                           KC_M,     KC_N,     KC_E,   TD(I_EQ),   KC_O,  TD(LBR_RBR),
   //|---------+---------+---------+---------+---------+---------|                     |---------+---------+---------+---------+---------+---------|
-      SFT_ESC,    KC_Z,    KC_X,      KC_C,    KC_D,   TD(V_HASH),                       KC_K,   TD(H_DOL), TD(CMA_SCLN), KC_DOT, KC_SLSH, TD(RPAR_RBR),
+      SFT_ESC,    KC_Z,    KC_X,      KC_C,    KC_D,   TD(V_HASH),                    TD(TD_KAT),   TD(H_DOL), TD(CMA_SCLN), KC_DOT, KC_SLSH, TD(LPAR_RPAR),
   //|---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------|
-                                          KC_LALT,  T_SYM, SFT_SPC,     SFT_ENT,   T_NAV, KC_RGUI
+                                        OSM(MOD_LALT),   T_NUM,  KC_SPC,       KC_ENT,  T_SYM,  KC_RGUI
                                       //`---------------------------------'  `-----------------------------'
 
   ),
-
-  [_NUMPAD] = LAYOUT_split_3x6_3(
+  
+  [_NUM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_EUR,  KC_P7,   KC_P8,    KC_P9,  XXXXXXX, _______,
+      XXXXXXX, MY_CUT,   KC_P7,   KC_P8,    KC_P9, KC_EUR,                       XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_P4,   KC_P5,    KC_P6, KC_PMNS, KC_PPLS,
+      MY_REDO, MY_CPY,  KC_P4,   KC_P5,    KC_P6, MY_UNDO,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_PMNS, KC_PPLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NLCK,                      KC_EQL,  KC_P1,   KC_P2,   KC_P3,  KC_PSLS, KC_PAST,
+      XXXXXXX, MY_PST, KC_P1,   KC_P2,    KC_P3, KC_NLCK,                      XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,   _______,   KC_P0, KC_DOT
+                                      TG(_NUM), _______, _______,   _______, _______,  KC_DOT
                                       //`--------------------------'  `--------------------------'
   ),
-
+  
   [_SYM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRV, KC_1,   KC_2,     KC_3,     KC_4,   KC_5,                          KC_6,    KC_7,    KC_8,   KC_9,     KC_0,   _______,
+       KC_GRV, KC_AMPR, KC_LABK,  MY_UNDO,  KC_RABK, KC_CIRC,                       KC_6,    KC_7,    KC_UP,   KC_9,     KC_0,   _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TILD, KC_EXLM, KC_AT,  KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC,  KC_AMPR, KC_ASTR, KC_UNDS, KC_MINS, KC_PLUS,
+      KC_TILD, KC_EXLM, MY_CUT, MY_PST, MY_CPY, KC_PERC,                      KC_UNDS,  KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS, KC_PLUS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      LCT_PRN, XXXXXXX, XXXXXXX, XXXXXXX, KC_EUR, XXXXXXX,                      KC_EQL, KC_PIPE,    KC_LT, KC_GT,   KC_BSLS,  RCT_PRN,
+      LCT_PRN, KC_PIPE, KC_LCBR, MY_REDO, KC_RCBR, KC_PSCR,                      KC_EQL, KC_PIPE,  KC_LT,    KC_GT, KC_BSLS, RCT_PRN,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, S_SYM,  _______,     KC_ENT, _______, KC_RALT
+                                          _______, _______,  _______,     KC_ENT, _______, TG(_SYM)
                                       //`--------------------------'  `--------------------------'
-  ),
-
-  [_NAV] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_PGUP,  KC_HOME, KC_UP,   KC_END, XXXXXXX, KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      LCT_PRN, KC_F11, KC_F12,   KC_INS, KC_PSCR, KC_CAPS,                      WIN_CLS, TAB_BCK, TAB_CLS, TAB_FWD,  XXXXXXX, RCT_PRN,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______,  KC_SPC,     KC_ENT, S_NAV, KC_RALT
-                                      //`--------------------------'  `--------------------------'
-  )
-
-};
+  ) 
+}; 
--- keyboards/crkbd/keymaps/slothoth/oled-icons.c
+++ keyboards/crkbd/keymaps/slothoth/oled-icons.c
@@ -27,39 +27,7 @@
 #define SYM 2
 #define FNC 3
 
-static void render_layer_state(uint8_t const state) {
-	static char const base_layer[] PROGMEM = {
-		0x20, 0x9a, 0x9b, 0x9c, 0x20,
-		0x20, 0xba, 0xbb, 0xbc, 0x20,
-		0x20, 0xda, 0xdb, 0xdc, 0x20, 0};
-	static char const numb_layer[] PROGMEM = {
-		0x20, 0x94, 0x95, 0x96, 0x20,
-		0x20, 0xb4, 0xb5, 0xb6, 0x20,
-		0x20, 0xd4, 0xd5, 0xd6, 0x20, 0};
-	static char const symb_layer[] PROGMEM = {
-		0x20, 0x97, 0x98, 0x99, 0x20,
-		0x20, 0xb7, 0xb8, 0xb9, 0x20,
-		0x20, 0xd7, 0xd8, 0xd9, 0x20, 0};
-	static char const func_layer[] PROGMEM = {
-		0x20, 0x9d, 0x9e, 0x9f, 0x20,
-		0x20, 0xbd, 0xbe, 0xbf, 0x20,
-		0x20, 0xdd, 0xde, 0xdf, 0x20, 0};
-
-	switch(state) {
-	case FNC: oled_write_P(func_layer, false); break;
-	case SYM: oled_write_P(symb_layer, false); break;
-	case NUM: oled_write_P(numb_layer, false); break;
-	default:  oled_write_P(base_layer, false);
-	}
-}
 
 // Primary modifier status display function
 void render_mod_status(void) {
-	uint8_t mods = get_mods();
-#ifndef NO_ACTION_ONESHOT
-	mods |= get_oneshot_mods();
-#endif
-	oled_set_cursor(0,6);
-	render_layer_state(get_highest_layer(layer_state|default_layer_state));
-	oled_set_cursor(0,11);
 }
