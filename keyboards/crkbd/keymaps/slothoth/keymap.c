ba48073f1172f4d71fbfe6f6475407d69f794a52
Sam Mayo <sdm2000@hw.ac.uk>
2022-07-21 12:04:07 +0100

    toggle on autoshift feature

--- keyboards/crkbd/keymaps/slothoth/keymap.c
+++ keyboards/crkbd/keymaps/slothoth/keymap.c
@@ -22,11 +22,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _COLEMAKDHM 0 
 #define _NUM 1
 #define _SYM 2
+#define _GAME 3 
 
 // Layer toggle and switch
 #define T_SYM TT(_SYM)
 #define T_NUM TT(_NUM)
-
 
 // Tap/mod keys
 // my mod taps
@@ -72,7 +72,7 @@ enum {
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     [Q_]        = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_UNDS),
-    [V_HASH]    = ACTION_TAP_DANCE_DOUBLE(KC_V, KC_HASH),
+    [V_HASH]    = ACTION_TAP_DANCE_DOUBLE(KC_V, LSFT(KC_TILD)),
     [U_QTE]     = ACTION_TAP_DANCE_DOUBLE(KC_U, KC_QUOT),
     [Y_AST]     = ACTION_TAP_DANCE_DOUBLE(KC_Y, KC_ASTR),
     [I_EQ]      = ACTION_TAP_DANCE_DOUBLE(KC_I, KC_EQL),
@@ -91,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------------.                     ,-----------------------------------------------------------.
       KC_ESC,    TD(Q_),   KC_W,      KC_F,    KC_P,     KC_B,                         TD(J_CLN),    KC_L, TD(U_QTE), TD(Y_AST), TD(PLS_MNS), KC_BSPC,
   //|---------+---------+---------+---------+---------+---------|                     |---------+---------+---------+---------+---------+---------|
-      CTL_TAB,    KC_A,    KC_R,      KC_S,    KC_T,     KC_G,                           KC_M,     KC_N,     KC_E,   TD(I_EQ),   KC_O,  TD(LBR_RBR),
+      SFT_ESC,    KC_A,    KC_R,      KC_S,    KC_T,     KC_G,                           KC_M,     KC_N,     KC_E,   TD(I_EQ),   KC_O,  TD(LPAR_RPAR),
   //|---------+---------+---------+---------+---------+---------|                     |---------+---------+---------+---------+---------+---------|
-      SFT_ESC,    KC_Z,    KC_X,      KC_C,    KC_D,   TD(V_HASH),                    TD(TD_KAT),   TD(H_DOL), TD(CMA_SCLN), KC_DOT, KC_SLSH, TD(LPAR_RPAR),
+      CTL_TAB,    KC_Z,    KC_X,      KC_C,    KC_D,   TD(V_HASH),                    TD(TD_KAT),   TD(H_DOL), TD(CMA_SCLN), KC_DOT, KC_SLSH, TD(LBR_RBR),
   //|---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------|
                                         OSM(MOD_LALT),   T_NUM,  KC_SPC,       KC_ENT,  T_SYM,  KC_RGUI
                                       //`---------------------------------'  `-----------------------------'
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX, MY_CUT,   KC_P7,   KC_P8,    KC_P9, KC_EUR,                       XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      MY_REDO, MY_CPY,  KC_P4,   KC_P5,    KC_P6, MY_UNDO,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_PMNS, KC_PPLS,
+      XXXXXXX, MY_CPY,  KC_P4,   KC_P5,    KC_P6, MY_UNDO,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_PMNS, KC_PPLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, MY_PST, KC_P1,   KC_P2,    KC_P3, KC_NLCK,                      XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -114,13 +114,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   
   [_SYM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRV, KC_AMPR, KC_LABK,  MY_UNDO,  KC_RABK, KC_CIRC,                       KC_2,    KC_3,    KC_UP,   KC_9,     KC_0,   _______,
+       KC_GRV, KC_AMPR, KC_LABK,  MY_UNDO,  KC_RABK, KC_CIRC,                    KC_2,    KC_3,    KC_UP,   KC_9,     KC_0,   _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TILD, KC_EXLM, MY_CUT, MY_PST, MY_CPY, KC_PERC,                      KC_UNDS,  KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS, KC_PLUS,
+      XXXXXXX, KC_EXLM, MY_CUT, MY_PST, MY_CPY, KC_PERC,                      KC_UNDS,  KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS, KC_PLUS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      LSFT, KC_PIPE, KC_LCBR, MY_REDO, KC_RCBR, KC_PSCR,                      KC_EQL, KC_PIPE,  KC_LT,    KC_GT, KC_BSLS, RCT_PRN,
+      XXXXXXX, KC_PIPE, KC_LCBR, MY_REDO, KC_RCBR, KC_PSCR,                      KC_EQL, KC_PIPE,  KC_LT,    KC_GT, KC_BSLS, TG(_GAME),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______,  _______,     KC_ENT, _______, TG(_SYM)
                                       //`--------------------------'  `--------------------------'
-  ) 
+  ),
+    [_GAME] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------------.                     ,-----------------------------------------------------------.
+      KC_ESC,    KC_Q,   KC_W,      KC_E,    KC_R,     KC_T,                              KC_Y,    KC_U,     KC_I,     KC_O,    KC_P,    KC_BSPC,
+  //|---------+---------+---------+---------+---------+---------|                     |---------+---------+---------+---------+---------+---------|
+      XXXXXXX,    KC_A,    KC_S,      KC_D,    KC_F,     KC_G,                           KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_ASTG,
+  //|---------+---------+---------+---------+---------+---------|                     |---------+---------+---------+---------+---------+---------|
+      XXXXXXX,    KC_Z,    KC_X,      KC_C,    KC_V,     KC_B,                           KC_N,     KC_M,    KC_COMMA,  KC_DOT, KC_SLSH,  TG(_GAME),
+  //|---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------|
+                                        OSM(MOD_LALT),   XXXXXXX,  KC_SPC,      KC_ENT, XXXXXXX,  KC_RGUI
+                                      //`---------------------------------'  `-----------------------------' 
+  )
 }; 
